261f918eb0f6fc1fee4aaf8ebed0d8f751592b3f
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2024-12-06 18:59:46 +0100

    convert to SSL and C++

--- examples/recorder/pipeline_raw_http/main/record_raw_http.cpp
+++ examples/recorder/pipeline_raw_http/main/record_raw_http.cpp
@@ -8,9 +8,11 @@
 */
 
 #include <string.h>
+#include <string>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "esp_tls.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 
@@ -66,6 +68,38 @@ audio_element_handle_t http_stream_writer;
 /**
  * FM's CODE        ↓↓↓↓
  */
+constexpr const char *pem =
+"-----BEGIN CERTIFICATE-----"
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh"
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4"
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu"
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY"
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc"
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+"
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U"
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW"
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH"
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC"
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv"
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn"
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn"
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw"
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI"
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV"
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq"
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL"
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ"
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK"
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5"
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur"
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC"
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc"
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq"
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA"
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d"
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc="
+"-----END CERTIFICATE-----";
 
 #define MIDDLE_BUTTON_GPIO GPIO_NUM_12
 
@@ -78,11 +112,11 @@ typedef struct {
 
 static void IRAM_ATTR button_isr_handler(void *arg)
 {
-    uint8_t gpio_num = (uint8_t)arg;
+    uint32_t gpio_num = (uint32_t)arg;
 
     button_event_t evt = {
-        .gpio_num = gpio_num,
-        .gpio_value = gpio_get_level(gpio_num)
+        .gpio_num = (uint8_t)gpio_num,
+        .gpio_value =(uint8_t) gpio_get_level((gpio_num_t)gpio_num)
     };
 
     xQueueSendFromISR(gpio_evt_queue, &evt, NULL);
@@ -108,7 +142,7 @@ static void button_task(void *arg)
                 audio_pipeline_reset_elements(pipeline);
                 audio_pipeline_terminate(pipeline);
 
-                audio_element_set_uri(http_stream_writer, CONFIG_SERVER_URI);
+                audio_element_set_uri(http_stream_writer, (std::string(CONFIG_SERVER_URI) + "/listen").c_str());
                 audio_pipeline_run(pipeline);
             } else if (evt.gpio_num == MIDDLE_BUTTON_GPIO && evt.gpio_value == 1) {
                 ESP_LOGI(TAG, "Middle button released, stopping recording.");
@@ -154,7 +188,7 @@ esp_err_t _http_stream_event_handle(http_stream_event_msg_t *msg)
         if (esp_http_client_write(http, len_buf, wlen) <= 0) {
             return ESP_FAIL;
         }
-        if (esp_http_client_write(http, msg->buffer, msg->buffer_len) <= 0) {
+        if (esp_http_client_write(http, (const char*)msg->buffer, msg->buffer_len) <= 0) {
             return ESP_FAIL;
         }
         if (esp_http_client_write(http, "\r\n", 2) <= 0) {
@@ -175,7 +209,7 @@ esp_err_t _http_stream_event_handle(http_stream_event_msg_t *msg)
 
     if (msg->event_id == HTTP_STREAM_FINISH_REQUEST) {
         ESP_LOGI(TAG, "[ + ] HTTP client HTTP_STREAM_FINISH_REQUEST");
-        char *buf = calloc(1, 64);
+        char *buf = (char*)calloc(1, 64);
         assert(buf);
         int read_len = esp_http_client_read(http, buf, 64);
         if (read_len <= 0) {
@@ -190,10 +224,19 @@ esp_err_t _http_stream_event_handle(http_stream_event_msg_t *msg)
     return ESP_OK;
 }
 
+extern "C" {
+    void app_main(void);
+}
+
 void app_main(void)
 {
     esp_log_level_set("*", ESP_LOG_WARN);
     esp_log_level_set(TAG, ESP_LOG_INFO);
+
+    esp_tls_set_global_ca_store(
+        reinterpret_cast<const unsigned char*>(pem),
+        strlen(pem)
+    );
 
     EXIT_FLAG = xEventGroupCreate();
 
@@ -216,8 +259,12 @@ void app_main(void)
     esp_periph_set_handle_t set = esp_periph_set_init(&periph_cfg);
 
     periph_wifi_cfg_t wifi_cfg = {
-        .wifi_config.sta.ssid = CONFIG_WIFI_SSID,
-        .wifi_config.sta.password = CONFIG_WIFI_PASSWORD,
+        .wifi_config = {
+            .sta = {
+                .ssid = CONFIG_WIFI_SSID,
+                .password = CONFIG_WIFI_PASSWORD,
+            },
+        },
     };
     esp_periph_handle_t wifi_handle = periph_wifi_init(&wifi_cfg);
 
@@ -244,15 +291,16 @@ void app_main(void)
     http_stream_cfg_t http_cfg = HTTP_STREAM_CFG_DEFAULT();
     http_cfg.type = AUDIO_STREAM_WRITER;
     http_cfg.event_handle = _http_stream_event_handle;
+    http_cfg.cert_pem = pem;
     http_stream_writer = http_stream_init(&http_cfg);
 
     ESP_LOGI(TAG, "[3.2] Create i2s stream to read audio data from codec chip");
     i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT_WITH_TYLE_AND_CH(
         CODEC_ADC_I2S_PORT,
         44100,
-        16,
+        (i2s_data_bit_width_t)16,
         AUDIO_STREAM_READER,
-        1
+        (i2s_slot_mode_t)1
     );
 
     i2s_cfg.type = AUDIO_STREAM_READER;
